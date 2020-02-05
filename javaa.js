39cb83035f7fe97e0a79b5a5ae3c6de1c192be4e
sffpll <59569678+sffpll@users.noreply.github.com>
2020-02-05 12:47:22 -0500

    first commit

---
+++ javaa.js
@@ -0,0 +1,56 @@
+
+
+
+//PROJECT 1
+function showText(){
+var text = document.querySelector("#text1")
+text.style.display = "block"
+/*console.log("working")*/
+}
+
+function hideText() {
+var text = document.querySelector("#text1")
+text.style.display = "none"
+}
+
+
+//PROJECT 2
+function showText2(){
+var text = document.querySelector("#text2")
+text.style.display = "block"
+/*console.log("working")*/
+}
+
+function hideText2() {
+var text = document.querySelector("#text2")
+text.style.display = "none"
+}
+
+
+
+//TEXT
+function showText3(){
+var text = document.querySelector("#text3")
+text.style.display = "block"
+/*console.log("working")*/
+}
+
+function hideText3() {
+var text = document.querySelector("#text3")
+text.style.display = "none"
+}
+
+
+//PROJECT #3
+function showText4(){
+var text = document.querySelector("#text4")
+text.style.display = "block"
+/*console.log("working")*/
+}
+
+function hideText4() {
+var text = document.querySelector("#text4")
+text.style.display = "none"
+}
+
+
